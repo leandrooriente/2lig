37b55d68b412b2c2a6d4818bff0d1ba983d14b1f
leandrooriente <leandroriente@gmail.com>
2017-02-12 18:28:11 -0200

    Add transition behaviour to burger navigation

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,5 @@
 var trigger = document.getElementById('hamburger'),
+    nav = document.getElementById('navigation'),
     isClosed = false;
 
 trigger.addEventListener('click', burgerTime, false);
@@ -7,10 +8,12 @@ function burgerTime() {
   if (isClosed === true) {
     trigger.classList.remove('is-open');
     trigger.classList.add('is-closed');
+    nav.classList.remove('opened');
     isClosed = false;
   } else {
     trigger.classList.remove('is-closed');
     trigger.classList.add('is-open');
+    nav.classList.add('opened');
     isClosed = true;
   }
 }
