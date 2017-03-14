76fdee08d215edb2f0788a5cd898bf451f730b28
leandrooriente <leandroriente@gmail.com>
2017-03-13 23:24:21 -0300

    Fix form

--- js/form.js
+++ js/form.js
@@ -1,23 +1,39 @@
 var contact = document.getElementById('contact');
 
 function handleSubmit(e) {
-  var name = document.getElementById('contact-name').value;
-  var email = document.getElementById('contact-email').value;
-  var tel = document.getElementById('contact-tel').value;
-  var message = document.getElementById('contact-message').value;
+  var name = document.getElementById('contact-name');
+  var email = document.getElementById('contact-email');
+  var tel = document.getElementById('contact-tel');
+  var message = document.getElementById('contact-message');
+  var button = document.getElementById('submit-button');
+  var alert = document.getElementById('alert');
 
-  console.log(name, email, tel, message);
+  alert.addEventListener('click', function() {
+    alert.classList.remove('show');
+  })
+
+  button.innerHTML = 'Enviando...'
+  button.disabled = true;
 
   axios({
     method: 'post',
     url: 'https://formspree.io/' + BASE_EMAIL,
     data: {
-      _subject: '2LIG - Novo contato de ' + name,
-      Nome: name,
-      Email: email,
-      Telefone: tel,
-      Mensagem: message
+      _subject: '2LIG - Novo contato de ' + name.value,
+      Nome: name.value,
+      Email: email.value,
+      Telefone: tel.value,
+      Mensagem: message.value
     }
+  })
+  .then(function(response) {
+    alert.classList.add('show');
+    button.disabled = false;
+    name.value = '';
+    email.value = '';
+    tel.value = '';
+    message.value = '';
+    button.innerHTML = 'Enviar';
   });
 
   e.preventDefault();
