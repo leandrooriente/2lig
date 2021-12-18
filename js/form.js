454a9271a3533a84533a2e312a9a1a66f833af06
Jonas Bolognim <jonas@2lig.com.br>
2021-12-19 00:34:39 +0100

    Updated Form

--- js/form.js
+++ js/form.js
@@ -1,5 +1,12 @@
 var contact = document.getElementById('contact');
 
+window.onload = function() {
+    var $recaptcha = document.querySelector('#g-recaptcha-response');
+    if($recaptcha) {
+        $recaptcha.setAttribute("required", "required");
+    }
+};
+
 function handleSubmit(e) {
   var name = document.getElementById('contact-name');
   var email = document.getElementById('contact-email');
@@ -7,12 +14,6 @@ function handleSubmit(e) {
   var message = document.getElementById('contact-message');
   var button = document.getElementById('submit-button');
   var alert = document.getElementById('alert');
-
-  var $recaptcha = document.querySelector('#g-recaptcha-response');
-  
-  if($recaptcha){
-    $recaptcha.setAttribute("required", "required");
-  }
 
   alert.addEventListener('click', function() {
     alert.classList.remove('show');
