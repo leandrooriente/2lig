3e7bdc86d048a0a362c96fdfd3bccfa5135edbbd
Jonas Bolognim <jonas@2lig.com.br>
2021-12-19 01:01:50 +0100

    Updated Form

--- js/form.js
+++ js/form.js
@@ -11,37 +11,35 @@ function handleSubmit(e) {
     var $recaptcha = document.querySelector('#g-recaptcha-response');
     if($recaptcha.value === '') {
         alert.value = 'Recapcha is required';
-        return false;
+    }else{
+        alert.addEventListener('click', function() {
+            alert.classList.remove('show');
+          })
+        
+          button.innerHTML = 'Enviando...'
+          button.disabled = true;
+        
+          axios({
+            method: 'post',
+            url: 'https://formspree.io/f/myyoyody',
+            data: {
+              _subject: '2LIG - Novo contato de ' + name.value,
+              Nome: name.value,
+              Email: email.value,
+              Telefone: tel.value,
+              Mensagem: message.value
+            }
+          })
+          .then(function(response) {
+            alert.classList.add('show');
+            button.disabled = false;
+            name.value = '';
+            email.value = '';
+            tel.value = '';
+            message.value = '';
+            button.innerHTML = 'Enviar';
+          });
     }
-
-  alert.addEventListener('click', function() {
-    alert.classList.remove('show');
-  })
-
-  button.innerHTML = 'Enviando...'
-  button.disabled = true;
-
-  axios({
-    method: 'post',
-    url: 'https://formspree.io/f/myyoyody',
-    data: {
-      _subject: '2LIG - Novo contato de ' + name.value,
-      Nome: name.value,
-      Email: email.value,
-      Telefone: tel.value,
-      Mensagem: message.value
-    }
-  })
-  .then(function(response) {
-    alert.classList.add('show');
-    button.disabled = false;
-    name.value = '';
-    email.value = '';
-    tel.value = '';
-    message.value = '';
-    button.innerHTML = 'Enviar';
-  });
-
   e.preventDefault();
 }
 
