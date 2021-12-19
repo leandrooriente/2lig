dcb82c50c0042f40b098410a3ece633fe8e1c63c
Jonas Bolognim <jonas@2lig.com.br>
2021-12-19 01:27:54 +0100

    Updated Form

--- js/form.js
+++ js/form.js
@@ -8,40 +8,42 @@ function handleSubmit(e) {
   var button = document.getElementById('submit-button');
   var alert = document.getElementById('alert');
  
-    var $recaptcha = document.querySelector('#g-recaptcha-response');
+    /* var $recaptcha = document.querySelector('#g-recaptcha-response');
     if($recaptcha.value === '') {
-        alert.innerHTML = 'Recapcha is required!'
-        alert.classList.add('show');
+        alert.value = 'Recapcha is required';
+        return false;
     }else{
-        alert.addEventListener('click', function() {
-            alert.classList.remove('show');
-          })
         
-          button.innerHTML = 'Enviando...'
-          button.disabled = true;
-        
-          axios({
-            method: 'post',
-            url: 'https://formspree.io/f/myyoyody',
-            data: {
-              _subject: '2LIG - Novo contato de ' + name.value,
-              Nome: name.value,
-              Email: email.value,
-              Telefone: tel.value,
-              Mensagem: message.value
-            }
-          })
-          .then(function(response) {
-            alert.innerHTML = 'Sua mensagem foi enviada com sucesso! <br /> Em breve entraremos em contato';
-            alert.classList.add('show');
-            button.disabled = false;
-            name.value = '';
-            email.value = '';
-            tel.value = '';
-            message.value = '';
-            button.innerHTML = 'Enviar';
-          });
+    } */
+
+  alert.addEventListener('click', function() {
+    alert.classList.remove('show');
+  })
+
+  button.innerHTML = 'Enviando...'
+  button.disabled = true;
+
+  axios({
+    method: 'post',
+    url: 'https://formspree.io/f/myyoyody',
+    data: {
+      _subject: '2LIG - Novo contato de ' + name.value,
+      Nome: name.value,
+      Email: email.value,
+      Telefone: tel.value,
+      Mensagem: message.value
     }
+  })
+  .then(function(response) {
+    alert.classList.add('show');
+    button.disabled = false;
+    name.value = '';
+    email.value = '';
+    tel.value = '';
+    message.value = '';
+    button.innerHTML = 'Enviar';
+  });
+
   e.preventDefault();
 }
 
